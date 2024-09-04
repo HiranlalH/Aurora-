f96e72faa0b45505ff1c6edef2a4b09fde94a2c2
AleenaMariya104 <aleena.sebastian@msds.christuniversity.in>
2024-09-04 10:02:39 +0530

    third push

--- Aurora/home/urls.py
+++ Aurora/home/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import home
+from .views import index,error,about,blog,contact,feature,how_to_use,product,testimonial
 
 urlpatterns = [
-    path('', home, name='index'),
+    path('', index, name='index'),
+    path('error/',error,name='error'),
+    path('about/',about,name='about'),
+    path('blog/',blog,name='blog'),
+    path('contact/',contact,name='contact'),
+    path('feature/',feature,name='feature'),
+    path('how_to_use/',how_to_use,name='how_to_use'),
+    path('product/',product,name='product'),
+    path('testimonial/',testimonial,name='testimonial')
+
 ]
