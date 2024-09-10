9afcde4186f573bc7ff775a28cba6596123fbccc
AleenaMariya104 <aleena.sebastian@msds.christuniversity.in>
2024-09-10 13:33:06 +0530

    alpush8

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -8,9 +10,36 @@ def error(request):
     return render(request,"error.html")
 
 def userlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        print(username)
+        print(password)
+        print(user)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect(index)
+        if user is None:
+            msg = "Please check the credentials carefully!"
+            return render(request, 'userlogin.html', {'msg':msg})
     return render(request,"userlogin.html")
 
 def userreg(request):
+    if request.method =="POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return render(request, 'userlogin.html')
+        else:
+            msg = "Username already exists. Try again!"
+            return render(request, 'userreg.html', {'msg':msg})
     return render(request,"userreg.html")
 
 
