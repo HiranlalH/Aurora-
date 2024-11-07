6734c9ccde3c687d70ca0327233448f1cbee9b33
AleenaMariya104 <aleena.sebastian@msds.christuniversity.in>
2024-11-07 22:45:21 +0530

    alnewpull

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Feedback, Contact
+from .models import Product, Feedback, Contact, Shipment
 
 admin.site.register(Product)
 admin.site.register(Feedback)
 admin.site.register(Contact)
+admin.site.register(Shipment)
